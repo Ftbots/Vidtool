6ec3b204af0702f20f24823817b58325443e67d9
Ftbots <fndmydvc0077@gmail.com>
2025-02-27 23:09:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'GreyMatters'
 
-
-if name == "__main__":
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) # Heroku provides the port
+    app.run(host='0.0.0.0', port=port)
